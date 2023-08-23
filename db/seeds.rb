d3ae672663c7c0fbf9313f4095880cac8562a2a7
Cedric Garcia <cgdm@tutanota.com>
2023-08-23 20:43:25 +0200

    erk;je

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create!({:name=>"Gaufrez", :address=>"Liege", :phone_number=>"0032445859565", :category=>"belgian"})
+Restaurant.create!({:name=>"Yunnan", :address=>"Beijing", :phone_number=>"0089745859565", :category=>"chinese"})
+Restaurant.create!({:name=>"Ratatouille", :address=>"Paris", :phone_number=>"0033345859565", :category=>"french"})
+Restaurant.create!({:name=>"Stronzo", :address=>"Milan", :phone_number=>"0039746859565", :category=>"italian"})
+Restaurant.create!({:name=>"Napolitana", :address=>"Roma", :phone_number=>"0039645859565", :category=>"italian"})
